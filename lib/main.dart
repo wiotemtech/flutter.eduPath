bbe2f891614f091feff6cd95a7b7c15475d3dfef
matia mathias <nyanzimathiaz@gmail.com>
2026-07-02 09:18:36 +0300

    cleared error

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        trfhjk;ljhfhjkll;lkm,
+        
       )
     );
   }
