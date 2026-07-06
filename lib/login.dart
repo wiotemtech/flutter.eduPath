757009d58170b76d1811671347567f62d7eadd0e
[Ajungo Caleb Christian] <[calebchristian.a24@gmail.com]>
2026-07-06 15:24:50 +0300

    Polished and finished Login screen UI

--- lib/login.dart
+++ lib/login.dart
@@ -45,166 +45,276 @@ class _LoginState extends State<Login> {
           fontWeight: FontWeight.bold,
         ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
+      body: Stack(
         children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                const Text(
-                  "Don't have an account? ",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
+          Positioned(
+            top: -120,
+            left: -120,
+            child: ShaderMask(
+              shaderCallback: (Rect bounds) {
+                return const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.white, Colors.white, Colors.transparent],
+                  stops: [0.0, 0.4, 0.8],
+                ).createShader(bounds);
+              },
+              blendMode: BlendMode.dstIn,
+              child: Container(
+                width: 450,
+                height: 380,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: RadialGradient(
+                    center: Alignment.topLeft,
+                    radius: 2.4,
+                    colors: [
+                      Color(0xFFE9D5FF),
+                      Color(0x99B56BE8),
+                      Colors.transparent,
+                    ],
+                    stops: [0.0, 0.3, 0.6],
                   ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    // Navigate to Register Screen using Get
-                    Get.to(() => const Register());
-                  },
-                  child: const Text(
-                    'sign up',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 90, 12, 236),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+
+          ListView(
+            padding: const EdgeInsets.all(16.0),
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account? ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // Navigate to Register Screen using Get
+                        Get.to(() => const Register());
+                      },
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 90, 12, 236),
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              TextField(
+                controller: contactController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  labelText: '+256',
+                  labelStyle: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  hintText: 'Enter your phone number',
+                  prefixIcon: const Icon(Icons.phone),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.grey, width: 1),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              TextField(
+                controller: passwordController,
+                //obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 66, 65, 65),
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  hintText: 'Enter your password',
+                  suffix: TextButton(
+                    onPressed: () {
+                      debugPrint('Forgot password tapped');
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: Size(0, 0),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    child: const Text(
+                      'Forgot',
+                      style: TextStyle(
+                        color: Color(0xFF5A0CEC),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-
-          const SizedBox(height: 25),
-
-          TextField(
-            controller: contactController,
-            keyboardType: TextInputType.phone,
-            decoration: InputDecoration(
-              labelText: '+256',
-              labelStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-              hintText: 'Enter your phone number',
-              prefixIcon: const Icon(Icons.phone),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: const BorderSide(color: Colors.grey, width: 1),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 40),
-
-          TextField(
-            controller: passwordController,
-            //obscureText: _obscurePassword,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              labelStyle: TextStyle(
-                color: Color.fromARGB(255, 66, 65, 65),
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-              ),
-              hintText: 'Enter your password',
-              prefixIcon: const Icon(Icons.lock),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: const BorderSide(color: Colors.grey, width: 1),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 24),
-          ElevatedButton(
-            onPressed: () {
-              final contact = contactController.text;
-              final password = passwordController.text;
-              debugPrint('Login pressed: $contact / $password');
-            },
-            child: const Text('Login'),
-          ),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {
-                  openWebsite('https://www.apple.com/');
-                },
-                icon: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(shape: BoxShape.circle),
-                  child: SvgPicture.asset(
-                    'assets/images/logo-apple.svg',
-                    width: 24,
-                    height: 24,
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.grey, width: 1),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
                   ),
                 ),
               ),
 
-              IconButton(
-                onPressed: () {
-                  openWebsite('https://www.google.com/');
-                },
-                icon: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(shape: BoxShape.circle),
-                  child: SvgPicture.asset(
-                    'assets/images/google-icon.svg',
-                    width: 24,
-                    height: 24,
+              const SizedBox(height: 80),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    height: 44,
+                    width: 120,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        final contact = contactController.text;
+                        final password = passwordController.text;
+                        debugPrint('Login pressed: $contact / $password');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFB56BE8),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          Container(
+                            width: 24,
+                            height: 24,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 1.2,
+                              ),
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: const Icon(
+                              Icons.arrow_forward_rounded,
+                              color: Colors.white,
+                              size: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
 
-              IconButton(
-                onPressed: () {
-                  openWebsite('https://www.facebook.com/');
-                },
-                icon: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(shape: BoxShape.circle),
-                  child: SvgPicture.asset(
-                    'assets/images/facebook_logo.svg',
-                    width: 24,
-                    height: 24,
+              const SizedBox(height: 80),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [const Text("Or Continue with")],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      openWebsite('https://www.apple.com/');
+                    },
+                    icon: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(shape: BoxShape.circle),
+                      child: SvgPicture.asset(
+                        'assets/images/logo-apple.svg',
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
                   ),
-                ),
+
+                  IconButton(
+                    onPressed: () {
+                      openWebsite('https://www.google.com/');
+                    },
+                    icon: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(shape: BoxShape.circle),
+                      child: SvgPicture.asset(
+                        'assets/images/google-icon.svg',
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                  ),
+
+                  IconButton(
+                    onPressed: () {
+                      openWebsite('https://www.facebook.com/');
+                    },
+                    icon: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(shape: BoxShape.circle),
+                      child: SvgPicture.asset(
+                        'assets/images/facebook_logo.svg',
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                  ),
+
+                  IconButton(
+                    onPressed: () {
+                      openWebsite('https://www.X.com/');
+                    },
+                    icon: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(shape: BoxShape.circle),
+                      child: SvgPicture.asset(
+                        'assets/images/twitter_x_logo.svg',
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                  ),
+                ],
               ),
 
-              IconButton(
-                onPressed: () {
-                  openWebsite('https://www.X.com/');
-                },
-                icon: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(shape: BoxShape.circle),
-                  child: SvgPicture.asset(
-                    'assets/images/twitter_x_logo.svg',
-                    width: 24,
-                    height: 24,
-                  ),
-                ),
-              ),
+              const SizedBox(height: 20),
             ],
           ),
-
-          const SizedBox(height: 20),
         ],
       ),
     );
